58a11fbe97547c0b578f39173b2b07ac2085db07
Rakibul Islam <mrislam4141@gmail.com>
2023-02-08 23:34:28 +0600

    react-frontend added

---
+++ react-frontend/src/components/DashBoardComponent.jsx
@@ -0,0 +1,20 @@
+import React,{ Component } from 'react'
+
+class DashBoardComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            students: []
+               
+        }
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default DashBoardComponent
---
+++ react-frontend/src/services/DashBoardServices.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/students";
+class DashBoardService {
+
+}
